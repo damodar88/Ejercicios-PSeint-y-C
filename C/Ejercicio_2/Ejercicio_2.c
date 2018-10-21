21020cc3e828597d1da7ac6d55efeaedcf24bc70
damodar88 <damodar88@icloud.com>
2018-10-21 12:45:01 -0300

    ejercicio 2 c

---
+++ C/Ejercicio_2/Ejercicio_2.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+    int intB,intA,intResultado;
+
+    printf("Ingrese un Numero--:");
+    scanf ("%d", &intA);
+    printf("Ingrese un Numero--:");
+    scanf ("%d", &intB);
+
+
+
+    intResultado=(intA+intB);
+
+    intResultado= pow(intResultado,2);
+
+    intResultado= intResultado / (3*intB);
+
+    printf("Resultado de la ecuacion es ---:%d",intResultado );
+
+	return 0;
+}
+
